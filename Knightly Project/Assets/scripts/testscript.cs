eb8eeb64441bb544f174be287029ecab48792c98
hishdafire <79139402+hishdafire@users.noreply.github.com>
2021-03-09 10:57:05 +0000

    added test script

---
+++ Knightly Project/Assets/scripts/testscript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour
+{
+    // this variable can be edited in unity 
+    public string startMessage;
+    public string spamMessage;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogError(startMessage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogWarning(spamMessage);
+    }
+}
